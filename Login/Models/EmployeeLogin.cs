a66f37c1a21e29231824aab51a2b4348b00fdff3
VenkatReactCode <67627606+VenkatReactCode@users.noreply.github.com>
2020-06-30 14:31:18 +0530

    Test1
    Test1

---
+++ Login/Models/EmployeeLogin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoginService.Models
+{
+    public partial class EmployeeLogin
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string EmployeeName { get; set; }
+        public string City { get; set; }
+        public string Department { get; set; }
+    }
+}
---
+++ Login/Models/LogInfo.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+namespace LoginService.Models
+{
+    public  class LogInfo
+    {
+        
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public string ResponseResult{get;set;}
+      
+    }
+}
