c8fc51c88bc6433e4cc9b551d2a90caf7af4d744
VenkatReactCode <67627606+VenkatReactCode@users.noreply.github.com>
2020-06-30 14:28:26 +0530

    Test
    Test

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+using System.Linq;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using LoginService.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
+using System.Text;
+namespace LoginService.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+       
+        [HttpPost]
+        public string PostLogin([FromBody]LogInfo log)
+        {
+            string response = "";
+            var user = AuthenticateUser(log);
+
+            
+                var tokenString = GenerateJSONWebToken(user);
+              return  response = Ok(new { token = tokenString }).Value.ToString();
+        }
+        private string GenerateJSONWebToken(LogInfo user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));    
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);    
+
+            var claims = new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, user.ResponseResult),
+                
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+    
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],    
+              null,    
+              claims,    
+              expires: DateTime.Now.AddMinutes(120),    
+              signingCredentials: credentials);    
+    
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        private LogInfo AuthenticateUser(LogInfo log)
+        {
+            LoginService.Models.TestDBContext db = new LoginService.Models.TestDBContext();
+
+
+           bool b =  db.EmployeeLogin.Any(s=>s.Email == log.Email && s.Password == log.Password);
+            if(b == true)
+            log.ResponseResult = "Success";
+            else
+                log.ResponseResult = "Failed";
+
+                return log;
+        }       
+        
+    }
+}
---
+++ Models/Studentmaster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LoginService.Models
+{
+    public partial class Studentmaster
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string RollNo { get; set; }
+        public string Class { get; set; }
+        public string Address { get; set; }
+    }
+}
